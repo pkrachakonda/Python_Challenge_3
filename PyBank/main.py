cffaee878b84e9c29b33541181da14b8c273a4e2
pkrachakonda <praveenkumar.rachakonda@gmail.com>
2023-08-09 22:27:05 +0800

    Added comments to python scripts and made some minor changes to scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,47 @@
-import os, csv
+import os, csv # Importing OS and CSV modules required for the analysis
 
+# Defining lists for storing data
 date = []
 values = []
 
+# Reading csv file located in the resource folder with csv module and saving the column data to lists
 with open(os.path.join("Resources", "budget_data.csv"), newline="") as csvfile:
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader:
         date.append(row["Date"])
         values.append(float(row["Profit/Losses"]))
 
+# Defining variables and a empty list
     total = 0
     change = 0
     count = 0
     diff = []
-    for i in range(0, len(values)):
-        total = total + values[i]
-        if i >= 1:
-            diff.append(values[i] - values[i - 1])
-            change = change + float(diff[i - 1])
-        count += 1
-    average_change = change / (count - 1)
 
-    lower_profit = 0
-    higher_profit = 0
-    for i in range(1, len(values) - 1):
+# Procedure for calculating total months, total and average change in profit/losses for the entire duration
+
+    for i in range(0, len(values)):
+        total = total + values[i]                       # Calculating the final sum of profit and losses
+        if i >= 1:
+            diff.append(values[i] - values[i - 1])      # estimating difference in value between two rows and adding that value to Diff list
+            change = change + float(diff[i - 1])        # estimating the total change in profit/losses at the current row
+        count += 1                                      # counting total months in the csv file
+    average_change = round((change / (count - 1)),2)    # estimating the average change and rounding the decimals to two places
+
+# Procedure for estimating Greatest and lowest increase in profit for the entire duration
+
+    lower_profit = 0                            # Declaration of variable
+    higher_profit = 0                           # Declaration of variable
+    for i in range(0, len(values) - 1):
         if float(diff[i]) < 0:
-            if float(diff[i]) < lower_profit:
-                lower_profit = float(diff[i])
-                day_low = date[i + 1]
-        elif float(diff[i]) > higher_profit:
-            higher_profit = float(diff[i])
-            day_high = date[i + 1]
+            if float(diff[i]) < lower_profit:   # checking to see if increase value is the lowest values
+                lower_profit = float(diff[i])   # assigning the lowest increase to "lowest_profit" variable, if "if" condition is true
+                day_low = date[i + 1]           # assigning the month value to "day_low" variable, if "if" condition is true
+        elif float(diff[i]) > higher_profit:    # checking to see if increase value is the highest values
+            higher_profit = float(diff[i])      # assigning the value to "highest_profit" variable, if "if" condition is true
+            day_high = date[i + 1]              # assigning the month value to "day_high" variable, if "if" condition is true
+
+csvfile.close() # Closing the csv file
+# Procedure/commands for printing the results to screen/terminal
 
 print("Financial Analysis \n")
 
@@ -40,25 +51,29 @@ print(f"Total Months: {count}")
 
 print(f"Total: ${int(total)}")
 
-print(f"Average Change: ${round(average_change,2)}")
+print(f"Average Change: ${average_change}")
 
 print(f"Greatest Increase in Profits: {day_high}  (${int(higher_profit)})")
 
 print(f"Greatest Decrease in Profits: {day_low}  (${int(lower_profit)})")
 
-with (open(os.path.join("Analysis", "PyBank_Analysis.txt"), 'w', newline = '') as csv_write):
+#  Writing the analysis results to a txt file and saving it to Analysis folder
+
+with (open(os.path.join("Analysis", "PyBank_Analysis.txt"), 'w', newline = '') as csv_write):  # Defining the path for saving the analysis results as txt file to a folder i.e. path to Analysis folder
     writer = csv.writer(csv_write, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
 
-    writer.writerow(['Financial Analysis\n'])
-    writer.writerow(['                      \n'])
-    writer.writerow(['----------------------------------------------\n'])
-    writer.writerow(['                      \n'])
+    writer.writerow(['Financial Analysis'])
+    writer.writerow(['                      '])
+    writer.writerow(['----------------------------------------------'])
+    writer.writerow(['                      '])
     writer.writerow(['Total Months: ' + str(count)])
-    writer.writerow(['                      \n'])
+    writer.writerow(['                      '])
     writer.writerow(['Total: $'+str(int(total))])
-    writer.writerow(['                      \n'])
-    writer.writerow(['Average Change: $'+ str(round(average_change, 2))])
-    writer.writerow(['                      \n'])
-    writer.writerow(['Greatest Increase in Profits: ' + day_high +'  '+'($'+str(int(higher_profit)) +')\n'])
-    writer.writerow(['                      \n'])
-    writer.writerow(['Greatest Decrease in Profits: ' + day_low +'  '+'($'+str(int(lower_profit)) +')\n'])
+    writer.writerow(['                      '])
+    writer.writerow(['Average Change: $'+ str(average_change)])
+    writer.writerow(['                      '])
+    writer.writerow(['Greatest Increase in Profits: ' + day_high +'  '+'($'+str(int(higher_profit)) +')'])
+    writer.writerow(['                      '])
+    writer.writerow(['Greatest Decrease in Profits: ' + day_low +'  '+'($'+str(int(lower_profit)) +')'])
+
+csv_write.close()      #  Closing the  text file after writing all the information
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,47 @@
-import os, csv
+import os, csv   # Importing OS and CSV modules required for the analysis
 
+# Defining lists for storing data
 cand = []
 ballots=[]
 cand_list = []
-with open(os.path.join("Resources","election_data.csv"), newline ='') as csvfile:
+
+# Reading csv file located in the resource folder with csv module and saving the column data to lists
+
+with open(os.path.join("Resources","election_data.csv"), newline ='') as csvfile: # Defining the path to the csv file folder i.e. path to Resource folder
     reader = csv.DictReader(csvfile)
     for row in reader:
         ballots.append(row["Ballot ID"])
         cand.append(row["Candidate"])
 
-    for x in cand:
-        if x not in cand_list:
-            cand_list.append(x)
+# Getting the unique candidate list
 
-    No_of_votes = [0 for row in cand_list]
+    for x in cand:
+        if x not in cand_list:            # checking to make sure that name is unique
+            cand_list.append(x)           # Adding the name to "cand_list" list
+
+# Estimating total number of votes polled as well as votes polled for each candidate
+
+    No_of_votes = [0 for row in cand_list]  # Defining a null list
     Total_votes = 0
     for i in range (0, len(ballots)):
         for j in range (0, len(cand_list)):
-            if cand[i] == cand_list [j]:
-                No_of_votes[j] = int(No_of_votes[j]) + 1
-                Total_votes += 1
+            if cand[i] == cand_list [j]: # checking the name of the candidate in csv file with the name in the candidate list
+                No_of_votes[j] = int(No_of_votes[j]) + 1  # assigning the votes to the candidate
+                Total_votes += 1 # Counting the total number of votes
 
-    percent_of_votes = []
+# Estimating the percentage of votes each candidate received and finding the winner candidate
+
+    percent_of_votes = [] # Declaring an empty list
     winner = 0
     for i in range(0, len(cand_list)):
-        percent_of_votes.append(round(((int(No_of_votes[i])/Total_votes) * 100),3))
-        if int(No_of_votes[i]) > winner:
-            winner = int(No_of_votes[i])
-            winner_cand = cand_list[i]
+        percent_of_votes.append(round(((int(No_of_votes[i])/Total_votes) * 100),3)) # Estimating percentage of votes each candidate received based on the total votes polled and rounding the percentage value to nearest third decimals
+        if int(No_of_votes[i]) > winner: # Checking if number of votes of current candidate is higher than the "winner" variable
+            winner = int(No_of_votes[i])  # if statement is TRUE, assigning the current number of votes value to "winner" variable
+            winner_cand = cand_list[i]   #  if statement is TRUE, assigning the current candidate list string value in the "winner_cand" variable
 
+csvfile.close()
+
+# Procedure/commands for printing the results to screen/terminal
 
 print("Election Results \n")
 
@@ -47,7 +60,9 @@ print(f"Winner: {winner_cand}\n")
 
 print("----------------------------------------------\n")
 
-with (open(os.path.join("Analysis", "Pyroll_Analysis.txt"), 'w', newline = '') as csv_write):
+#  Writing the analysis results to a txt file and saving it to Analysis folder
+
+with (open(os.path.join("Analysis", "Pyroll_Analysis.txt"), 'w', newline = '') as csv_write):   # Defining the path for saving the analysis results as txt file to a folder i.e. path to Analysis folder
     writer = csv.writer(csv_write, delimiter = ',', quoting = csv.QUOTE_MINIMAL)
 
     writer.writerow(['Election Results'])
@@ -66,3 +81,5 @@ with (open(os.path.join("Analysis", "Pyroll_Analysis.txt"), 'w', newline = '') a
     writer.writerow(['Winner:'+' '+ str(winner_cand)])
     writer.writerow(['                      '])
     writer.writerow(['----------------------------------------------'])
+
+csv_write.close()      #  Closing the  text file after writing all the information
